209f95aa74f3c0e275d2c5b0c6130b2b84a6b45c
yazan6546 <1210145@student.birzeit.edu>
2025-05-12 09:52:33 +0300

    Add build script to initialize and configure the build system
    This script ensures a `build` directory exists and sets up the project using CMake with debug configuration. It incorporates Conan for dependency management via a top-level include file.

---
+++ build.sh
@@ -0,0 +1,9 @@
+if [ ! -d build ]
+then
+  echo "Creating build directory..."
+  mkdir -p build
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build \
+      -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=conan_provider.cmake \
+      -DCONAN_COMMAND=conan
