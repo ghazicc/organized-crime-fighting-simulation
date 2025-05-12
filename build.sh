67e15d9f96dc2ca06c277ce571918955872ccdca
yazan6546 <1210145@student.birzeit.edu>
2025-05-12 14:45:43 +0300

    Enable testing in build scripts and refine CMake configuration
    Testing is now explicitly enabled in both CI workflows and build scripts. Adjustments include setting `BUILD_TESTING=ON` and simplifying configuration-related logic in `CMakeLists.txt`. Additionally, `.gitignore` was updated to include common CMake-generated files.

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ then
   mkdir -p build
 fi
 
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build \
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B build \
       -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=conan_provider.cmake \
-      -DCONAN_COMMAND=conan
+      -DCONAN_COMMAND=conan -DBUILD_TESTING=ON
