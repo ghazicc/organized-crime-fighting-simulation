4d07059a4560b4fad770b1bc288fd26c622eeb04
ghazicc <ghazihajqassem@gmail.com>
2025-05-22 18:01:26 +0300

    Refactor message handling: update message structure and implement message queue functions

--- include/message.h
+++ include/message.h
@@ -1,16 +1,18 @@
 #ifndef POLICE_REPORT
 #define POLICE_REPORT
 
+#include <stdint.h>  // For uint8_t type
+
 // define message queue keys
-#define POLICE_REPORT_KEY 0xCAFEBABE
+#define POLICE_GANG_KEY 0x1234
+#define MESSAGE_SIZE sizeof(Message) - sizeof(long)
 
 typedef struct {
-    long mtype; // Message type
-    int target_type;
-    float suspicion_level;
-    int agent_id;
-} PoliceReport;
-
-
-
+    long mtype;
+    uint8_t mode;
+    union {
+        float knowledge;
+        int agent_id;
+    } MessageContent;
+} Message;
 #endif // BAKERY_MESSAGE_H
--- src/utils/message_queue_utils.c
+++ src/utils/message_queue_utils.c
@@ -6,3 +6,22 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+
+int create_message_queue() {
+    int msgid = msgget(POLICE_GANG_KEY, IPC_CREAT | 0666);
+    if (msgid == -1) {
+        perror("msgget");
+        return -1;
+    }
+    return msgid;
+}
+
+int send_message(int msgid, Message *message) {
+    if (msgsnd(msgid, message, MESSAGE_SIZE, 0) == -1) {
+        perror("msgsnd");
+        return -1;
+    }
+    return 0;
+}
+
+
