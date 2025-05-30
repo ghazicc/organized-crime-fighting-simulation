326508c80aad661e48311bb6481f8432175d2b13
ghazicc <ghazihajqassem@gmail.com>
2025-05-30 23:52:04 +0300

    Simplify build script by removing unnecessary cmake options and commands

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ then
   mkdir -p build
 fi
 
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build \
-      -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=conan_provider.cmake \
-      -DCONAN_COMMAND=conan -DBUILD_TESTING=ON
+cd build && cmake ..
